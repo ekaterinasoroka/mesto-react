1a6e35f7b2ce15aaae0e7d9c10c7736758834c26
Ekaterina Soroka <marusyakate@mail.ru>
2022-07-16 15:26:10 +0300

    Правки

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,9 @@ import logo from '../images/logo.svg';
 
 function Header() {
   return (
-		<header className="header">
-			<img className="header__logo" src={logo} alt="Логотип"/>
-		</header>
+    <header className="header">
+      <img className="header__logo" src={logo} alt="Логотип"/>
+    </header>
   );
 }
 
--- src/components/Main.js
+++ src/components/Main.js
@@ -3,73 +3,71 @@ import api from '../utils/api.js';
 import Card from './Card.js';
 
 function Main({onEditProfile, onAddPlace, onEditAvatar, onCardClick}) {
-	const [userName, setUserName] = useState('');
-	const [userDescription, setUserDescription] = useState('');
-	const [userAvatar, setUserAvatar] = useState('');
-	const [cards, setCards] = useState([]);
+  const [userName, setUserName] = useState('');
+  const [userDescription, setUserDescription] = useState('');
+  const [userAvatar, setUserAvatar] = useState('');
+  const [cards, setCards] = useState([]);
 
-	function getApiStartPage() {
-		Promise.all([api.getInfoUsers(), api.getCards()])
-			.then(([userInfo, card]) => {
-				setUserName(userInfo.name);
-				setUserDescription(userInfo.about);
-				setUserAvatar(userInfo.avatar);
-				setCards(card);
-			})
-			.catch((err) => console.log(err));
-	}
+  function getApiStartPage() {
+    Promise.all([api.getInfoUsers(), api.getCards()])
+      .then(([userInfo, card]) => {
+        setUserName(userInfo.name);
+        setUserDescription(userInfo.about);
+        setUserAvatar(userInfo.avatar);
+        setCards(card);
+      })
+      .catch((err) => console.log(err));
+    }
 
-	useEffect(() => {
-		getApiStartPage();
-	}, []);
+    useEffect(() => {
+      getApiStartPage();
+    }, []);
 
-	return (
-		<>
-			<section className="profile">
-				<a 
-					className="profile__avatar-container" 
-					href="##" alt="аватар" 
-					onClick={onEditAvatar}
-				>
-					<img 
-						className="profile__avatar" 
-						src={userAvatar} 
-						alt="аватар"
-					/>
-				</a>
+    return (
+      <>
+        <section className="profile">
+          <a 
+            className="profile__avatar-container" 
+            href="##" alt="аватар" 
+	    onClick={onEditAvatar}
+	  >
+            <img 
+              className="profile__avatar" 
+	      src={userAvatar} 
+	      alt="аватар"
+	    />
+	 </a>
 					
-				<div className="profile__info">
-					<h1 className="profile__title" name="name">{userName}</h1>
-					<button 
-						className="profile__edit-button" 
-						type="button" 
-						onClick={onEditProfile}
-					>
-					</button>
-					<p 
-						className="profile__subtitle" 
-						name="about">
-							{userDescription}
-					</p>
-				</div>
-				<button className="profile__add-button" type="button" onClick={onAddPlace}></button>
-			</section>
+	 <div className="profile__info">
+	   <h1 className="profile__title" name="name">{userName}</h1>
+	   <button 
+	     className="profile__edit-button" 
+	     type="button" 
+	     onClick={onEditProfile}
+	   >
+	   </button>
+	   <p 
+	     className="profile__subtitle" 
+	     name="about">
+	       {userDescription}
+	   </p>
+	 </div>
+	 <button className="profile__add-button" type="button" onClick={onAddPlace}></button>
+       </section>
 
-			<div className="elements">
-				{cards.map((item) => {
-					return (
-						<Card
-							key={item._id}
-							card={item}
-							onCardClick={onCardClick}
-						/>
-					)
-				})}
-			</div>
-		</>
-	)
+       <div className="elements">
+         {cards.map((item) => {
+	   return (
+	     <Card
+	       key={item._id}
+	       card={item}
+	       onCardClick={onCardClick}
+	     />
+	   )
+	 })}
+       </div>
+     </>
+  )
 }
 
 export default Main;
-
-
