61a62eb34478759aec0e8875d6850834ccb86415
Ekaterina Soroka <marusyakate@mail.ru>
2022-07-23 21:39:04 +0300

    Pr11 rewiev

--- src/components/Main.js
+++ src/components/Main.js
@@ -10,45 +10,44 @@ function Main({onEditProfile, onAddPlace, onEditAvatar, onCardClick, cards, onCa
           <a 
             className="profile__avatar-container" 
             href="##" alt="аватар" 
-	    			onClick={onEditAvatar}
-	  			>
+	    onClick={onEditAvatar}
+	  >
             <img 
               className="profile__avatar" 
-	      			src={currentUser?.avatar} 		
-	      			alt="аватар"
-	    			/>
-					</a>
-					
-					<div className="profile__info">
-						<h1 className="profile__title" name="name">{currentUser?.name}</h1>
-						<button 
-							className="profile__edit-button" 
-							type="button" 
-							onClick={onEditProfile}
-						>
-						</button>
-						<p 
-							className="profile__subtitle" 
-							name="about">
-								{currentUser?.about}
-						</p>
-					</div>
-					<button className="profile__add-button" type="button" onClick={onAddPlace}></button>
-				</section>
+	      src={currentUser?.avatar} 		
+	      alt="аватар"
+	    />
+	  </a>
+	  <div className="profile__info">
+	    <h1 className="profile__title" name="name">{currentUser?.name}</h1>
+	    <button 
+	      className="profile__edit-button" 
+	      type="button" 
+	      onClick={onEditProfile}
+	    >
+	    </button>
+	    <p 
+	      className="profile__subtitle" 
+	      name="about">
+	      {currentUser?.about}
+	    </p>
+	  </div>
+	  <button className="profile__add-button" type="button" onClick={onAddPlace}></button>
+	</section>
 
-				<div className="elements">
-					{cards.map((item) => {
-						return (
-							<Card
-								key={item._id}
-								card={item}
-								onCardClick={onCardClick}
-								onCardLike={onCardLike}
-								onCardDelete={onCardDelete}
-							/>
-						)
-					})}
-				</div>
+	<div className="elements">
+	  {cards.map((item) => {
+	    return (
+	      <Card
+		key={item._id}
+		card={item}
+		onCardClick={onCardClick}
+		onCardLike={onCardLike}
+		onCardDelete={onCardDelete}
+	      />
+	    )
+	  })}
+	</div>
      </>
   )
 }
