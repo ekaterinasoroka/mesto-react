4a86c4f9e0f5cbf11c9e5929dc4d14250ded70b8
Ekaterina Soroka <marusyakate@mail.ru>
2022-08-02 11:05:47 +0300

    проект12

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,10 @@
 import logo from '../images/logo.svg';
 
-function Header() {
+function Header({userData: {email}}) {
   return (
     <header className="header">
       <img className="header__logo" src={logo} alt="Логотип"/>
+      <p className="header__info-user">{email}</p>
     </header>
   );
 }
